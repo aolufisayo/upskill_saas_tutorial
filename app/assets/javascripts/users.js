816543de82e856dc434da9de5a44525f7a925f9c
olufisayo akinmoju <juliematheson95@gmail.com>
2017-10-13 10:21:11 +0000

    added stripe response handlers and erro handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,23 +11,63 @@ $(document).on('turbolinks.load', function(){
   submitBtn.click(function(event){
   //we'll prevent default form submission behaviour.
     event.preventDefault();
+    submitBtn.val("processing").prop('disabled', true);
     //collect the credit card fields
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
         
-  //send the card info to stripe
-  Stripe.createToken({
-    number: ccNum,
-    cvc: cvcNum,
-    exp_month:expMonth,
-    exp_year: expYear
-  }, stripeResponseHandler)
+        
+    //use stripe js library to check for card errors.
+    var error = false;
+    
+    //validate card number
+    
+    if (!Stripe.validateCardNumber(ccNum)){
+      error = true
+      alert("The credit card number appears to be invalid")
+    }
+    // validate cvc
+    if (!Stripe.validateCVC(cvcNum)){
+      error = true
+      alert("The CVC number appears to be invalid")
+    }
+    //validate expiry 
+    if (!Stripe.validateExpiry(expMonth, expYear)){
+      error = true
+      alert("The expiration date appears to be invalid")
+    }
+   
+    if (error) {
+      //if there are card errors , dont send to stripe
+      submitBtn.prop("disabled", false).val("Sign up");
+      
+    } else {
+      //send the card info to stripe
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month:expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    
+    }
+    
+    return false   
   
   });
   //stripe will return a card token
-  //inject card token as hidden fields into form
-  //submit form to our rails app.
+  function stripeResponseHandler(status, response){
+    var token = response.id
+    
+     //inject card token as hidden fields into form
+    theForm.append($('<input type="hidden" name="user[stripe_card_token]"> ').val(token) );
+    
+    //submit form to our rails app.
+    theForm.get(0).submit();
+  }
+ 
+ 
 
 })
